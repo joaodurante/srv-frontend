9b71234e92fe9dac037c840c0869cdf5ccaf2e68
Joao Pedro de Munno Durante <joao.durante@venturus.org.br>
2022-06-25 15:56:09 -0300

    update home css attributes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,21 +6,21 @@ import './Home.css';
 export default function Home() {
     return(
         <Box sx={{ display: 'flex' }}>
-        <AppMenu menu={true} auth={true} />
-        <CssBaseline />
-        <Box sx={{ 
-            flexGrow: 1, 
-            height: '100vh', 
-            overflow: 'auto', 
-            'background-color': '#eeeeee',
-            display: 'flex',
-            'justify-content': 'center',
-            'align-items': 'center'
-        }}>
-            <div>
-                <h1>Bem Vindo!</h1>
-            </div>
+            <AppMenu menu={true} auth={true} />
+            <CssBaseline />
+            <Box sx={{ 
+                flexGrow: 1, 
+                height: '100vh', 
+                overflow: 'auto', 
+                backgroundColor: '#eeeeee',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <div>
+                    <h1>Bem Vindo!</h1>
+                </div>
+            </Box>
         </Box>
-    </Box>
     )
 }
